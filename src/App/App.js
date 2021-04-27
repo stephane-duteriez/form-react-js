b5584d5e7e67112a81a367f51b8fd3098d78cadc
stephane-duteriez <stephane.duteriez@gmail.com>
2021-04-27 16:53:59 +0200

    mise en place de class

--- src/App/App.js
+++ src/App/App.js
@@ -1,26 +1,34 @@
 
+import React from 'react';
 import './App.css';
 import Button from './components/Button/Button'
-function App() {
-  return (
-    <div className="App">
-      <Button 
-      label="Ok" 
-      couleurDeFond={'green'}
-      onclick={(evt)=>{
-        console.log('J\'ai été cliqué');
-      }} />
-      <Button 
-        label="Canceal"
-        onclick={(evt)=>{
-          console.log('J\'ai été cliqué');
-        }}
-        style={{textDecoration:'underline'}}
-        couleurDeFond={'red'}
-        taillePolice={24}
-      />
-    </div>
-  );
-}
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter:-1,
+      lastClickedTime:null
+    }
+  }
+  componentDidMount() {
+    this.setState({
+      counter : 0
+    })
+  }
+  render() {
+    return <div className="App">
+        <div style={{padding:'20px'}}>Les boutons on été cliqués : {this.state.counter} fois <br/>
+        {this.state.lastClickedTime && 'dernier click ' + this.state.lastClickedTime.toISOString()}</div>
+      <Button label="btn 1" onclick={(evt)=>{
+        console.log("test click");
+        this.setState({counter: this.state.counter + 1, 
+        lastClickedTime: new Date()});
+        }}/>
+      <Button label="btn 2" couleurDeFond="red" onclick={()=>{
+        this.setState({counter: this.state.counter + 1});
+        }}/>
+    </div>
+  }
+}
 export default App;
