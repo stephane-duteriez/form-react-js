c443b1c8b7a7fa159f37b23f3bee844c7fc78786
stephane-duteriez <stephane.duteriez@gmail.com>
2021-04-27 14:52:50 +0200

    avant ajout button

---
+++ src/App/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+const uneVar ='Hello'
+function App() {
+  return (
+    <div className="App">
+      {uneVar}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/App/components/Button/Button.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Button() {
+    
+    return <div>
+        Hello world
+    </div>
+}
+
+export default Button;
