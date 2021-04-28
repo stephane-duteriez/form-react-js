0333f165480b6f96125e79fdf561608bd467b645
stephane-duteriez <stephane.duteriez@gmail.com>
2021-04-28 15:34:37 +0200

    generalisation imageId

--- src/App/components/MemeViewer/MemeViewer.js
+++ src/App/components/MemeViewer/MemeViewer.js
@@ -4,7 +4,7 @@ import styles from './MemeViewer.module.scss';
 
 const MemeViewer = (props) => (
   <svg className={styles.MemeViewer} data-testid="MemeViewer" viewBox="0 0 4000 6000">
-    <image x={0} y={0} href={props.currentMeme.image} />
+    <image x={0} y={0} href={props.currentMeme.imageId} />
     <text x={props.currentMeme.text.x} y={props.currentMeme.text.y + 200} className={styles.text} style={{
           fill : ( props.currentMeme.text.color || "#FFFFFF"),
           textDecoration : (props.currentMeme.text.underliner?'underline': 'none' ),
