8760553a8fcaca1610c0124ca8b1082cb83db189
stephane-duteriez <stephane.duteriez@gmail.com>
2021-04-28 11:08:34 +0200

    form meme

--- src/App/App.js
+++ src/App/App.js
@@ -1,37 +1,28 @@
 
 import React from 'react';
-import './App.css';
-import Button from './components/Button/Button'
-import MemeForm from './components/MemeForm/MemeForm'
+import styles from './App.css';
+import MemeForm, {initialState} from './components/MemeForm/MemeForm'
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      counter:-1,
-      lastClickedTime:null
+      currentMeme : {}
     }
   }
   componentDidMount() {
     this.setState({
-      counter : 0
+      currentMeme : initialState
     })
   }
   render() {
     return <div className="App">
-        <div style={{padding:'20px'}}>Les boutons on été cliqués : {this.state.counter} fois <br/>
-        {this.state.lastClickedTime && 'dernier click ' + this.state.lastClickedTime.toISOString()}</div>
-        <MemeForm>
-
-        </MemeForm><br/>
-      <Button label="btn 1" onclick={(evt)=>{
-        console.log("test click");
-        this.setState({counter: this.state.counter + 1, 
-        lastClickedTime: new Date()});
-        }}/>
-      <Button label="btn 2" couleurDeFond="red" onclick={()=>{
-        this.setState({counter: this.state.counter + 1});
-        }}/>
+      <MemeForm formContent={this.state.currentMeme} onSubmit={(currentMeme)=>this.setState({currentMeme : currentMeme})}></MemeForm>
+      <div class={styles.show}>
+        <span>
+          {JSON.stringify(this.state.currentMeme)}
+          </span>
+      </div>
     </div>
   }
 }
--- src/App/components/MemeForm/MemeForm.js
+++ src/App/components/MemeForm/MemeForm.js
@@ -1,34 +1,86 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import styles from './MemeForm.module.css';
+import Button from '../Button/Button';
 
-const MemeForm = () => {
-  const [formContent, setformContent] = useState({
-    name : 'tjs', count:0, color:'#000000'
-  }); 
+const initialState = {
+  name : '',
+  text:{x:0,y:0,value:'', underline:false, bold:false, color:"#555555"},
+  image:''
+};
+const MemeForm = (props) => {
+  const [formContent, setformContent] = useState(initialState); 
   return (
     <form className={styles.MemeForm} data-testid="MemeForm">
-      <input type="text" placeholder="nom de la formation"
+      <label htmlFor="name">Nom du meme : </label><br/>
+      <input type="text" placeholder="nom du meme" id="name"
       value={formContent.name}
         onChange={(evt)=>{
           setformContent({...formContent, name:evt.target.value})
         }}
       /><br/>
-      <input type="number" placeholder="nb de participant" value={formContent.count}
-        onChange={(evt)=>{
-          setformContent({...formContent, count:parseInt(evt.target.value, 10)})
-        }}
-      /><br/>
-      <input type="color" value={formContent.color} onChange={(evt)=>{
-        setformContent({...formContent, color:evt.target.value})
-      }}/><br/>
-      <input type="submit" value="Soumettre"/><br/>
-      voici le contenu actuellement saisie :
-      <span style={{color:formContent.color}}>
+      <div class={styles.text}>
+        <label htmlFor="text">Text : </label>
+        <input type="text" value={formContent.text.value} id="text"
+          onChange={(evt)=>{
+            setformContent({...formContent, text:{...formContent.text,value:evt.target.value}});
+          }}></input><br/>
+        <div class={styles.inlineContainer}>
+          <label>X : </label>
+          <input  type="number" value={formContent.x}
+            onChange={(evt)=>{
+              setformContent({...formContent, text:{...formContent.text, x:parseInt(evt.target.value)}});
+            }}></input>
+          <label>Y : </label>
+          <input  type="number" value={formContent.y}
+            onChange={(evt)=>{
+              setformContent({...formContent, text:{...formContent.text, y:parseInt(evt.target.value)}});
+            }}></input>
+        </div>
+        <label id="color">Color : </label>
+        <input type="color" value={formContent.color} onChange={(evt)=>{
+          setformContent({...formContent, color:evt.target.value})
+        }}/><br/>
+        <div class={styles.inlineContainer}>
+          <label>
+            <input forhtml="underline" type="checkbox" checked={formContent.text.underliner}
+            onChange={(evt)=>{
+              setformContent({...formContent, text:{...formContent.text, underliner: evt.target.checked}});
+            }}></input>
+            Underline
+          </label>
+          <label>
+            <input forhtml="bold" type="checkbox" checked={formContent.text.bold}
+            onChange={(evt)=>{
+              setformContent({...formContent, text:{...formContent.text, bold: evt.target.checked}})
+            }}></input>
+            Bold
+          </label>
+        </div>
+       
+      </div>
+      <div>
+        <label htmlFor="image">Image : </label>
+        <select id="image" onChange={(evt)=>{
+          setformContent({...formContent, image:evt.target.value})
+        }} value={formContent.image}>
+          <option value="img/test.jpg">test.jpg</option>
+          <option value="img/test2.jpg">test2.jpg</option>
+        </select>
+      </div>
+      <div className={styles.show}>
+      <span style={{color:formContent.text.color}}>
       {JSON.stringify(formContent)}
       </span>
-       
+      </div>
 
+      <Button label="reset" color="#FFFFFF" onclick={(evt)=>{
+        setformContent(initialState)
+      }}/>
+      <Button label="Ok" color="#FFFFFF" onclick={(evt)=>{
+        props.onSubmit(formContent);
+      }}/><br/>
+      
     </form>
   );
 };
@@ -38,3 +90,4 @@ MemeForm.propTypes = {};
 MemeForm.defaultProps = {};
 
 export default MemeForm;
+export {initialState};
